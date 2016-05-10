03324a1469a1f8fd1be7fb7ee383b4e12098d0c1
Enrico Yang <enrico@gexne.com>
2016-05-10 11:38:58 +0800

    add arguements

--- cont_monkey.sh
+++ cont_monkey.sh
@@ -1,39 +1,131 @@
 #!/bin/sh
 
-DEVICE_SN='CDRTCYC3Y16CFDXG'
+#DEVICE_SN='CDRTCYC3Y16CFDXG'
 #ADB_DEVICE="adb -s "$DEVICE_SN
 ADB_DEVICE="adb"
 #echo $ADB_DEVICE
 #MONKEY_SEED='-s 1234567890'
-RUN_TIME=500000
+RUN_TIME=1
 EVENT_EXERCUTED=0
 
-if [ $# -gt 1 ]
-then
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	echo "!!         输入参数不正确         !!"
-	echo "!!      导出失败，未生成文件      !!"
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+function normalCat(){
+	echo normal finish
+	mkdir -p normal
+	LOGPATH=normal/
+	mv monkey_log_$DATETIME.txt $LOGPATH
+}
 
-	exit 1
-fi
-if [ $# -eq 1 ]
-then
-	DATETIME=$(echo $1 | cut -d _ -f 3 | cut -d . -f 1)
-	echo "*****analyse monkey log*****"
-	RANDOM_SEED=$(cat monkey_log_$DATETIME.txt | grep -o 'seed=[0-9]*' | cut -d = -f 2)
-	EVENT_COUNT=$(cat monkey_log_$DATETIME.txt | grep -o 'at event [0-9]*' | awk '{print $3}')
-	if test -z "$EVENT_COUNT" ;then
-		EVENT_COUNT=0
-		mkdir -p unknown
-		cd unknown
+function unknownCat(){
+	echo unknown reason interruption
+	mkdir -p unknown
+	cd unknown
+	mkdir -p $DATETIME
+	cd ..
+	LOGPATH=unknown/$DATETIME/
+	mv monkey_log_$DATETIME.txt $LOGPATH
+}
+
+function anrCat(){
+	echo error cause:ANR
+	mkdir -p ANR
+	cd ANR
+	ERROR_POS=$(echo $ERROR_INFO | cut -d \/ -f 2 | cut -d \) -f 1 | cut -d . -f 2)
+	ERROR_MODULE=$(echo $ERROR_INFO | awk '{print $3}')
+	mkdir -p $ERROR_MODULE
+	cd $ERROR_MODULE
+	mkdir -p $ERROR_POS
+	cd $ERROR_POS
+	mkdir -p $DATETIME
+	cd ..
+	cd ..
+	cd ..
+	LOGPATH=ANR/$ERROR_MODULE/$ERROR_POS/$DATETIME/
+	mv monkey_log_$DATETIME.txt $LOGPATH
+}
+
+function crashCat(){
+	echo error cause:CRASH
+	ERROR_MODULE=$(cat monkey_log_$DATETIME.txt | grep 'CRASH:' | awk '{printf $3}')
+	ERROR_POS=$(cat monkey_log_$DATETIME.txt | grep '.java:' | head -1 | awk '{printf $3}')
+	if test -n "$ERROR_POS" ;then
+		echo 'error cause:CRASH'
+		echo 'error position:'$ERROR_POS
+		mkdir -p CRASH
+		cd CRASH
+		mkdir -p $ERROR_MODULE
+		cd $ERROR_MODULE
+		mkdir -p $ERROR_POS
+		cd $ERROR_POS
 		mkdir -p $DATETIME
 		cd ..
-		mv monkey_log_$DATETIME.txt unknown/$DATETIME
-		$ADB_DEVICE pull /data/tombstones/ unknown/$DATETIME/
-		$ADB_DEVICE pull /data/system/dropbox/ unknown/$DATETIME/
-		$ADB_DEVICE shell rm -f /data/tombstones/*
-		$ADB_DEVICE shell rm -f /data/system/dropbox/*
+		cd ..
+		cd ..
+		LOGPATH=CRASH/$ERROR_MODULE/$ERROR_POS/$DATETIME/
+		mv monkey_log_$DATETIME.txt $LOGPATH
+	fi
+}
+
+function pullLogAndRemove(){
+
+	$ADB_DEVICE pull /data/tombstones/ $LOGPATH
+	$ADB_DEVICE pull /data/system/dropbox/ $LOGPATH
+	$ADB_DEVICE shell rm -f /data/tombstones/*
+	$ADB_DEVICE shell rm -f /data/system/dropbox/*
+	$ADB_DEVICE shell /system/bin/screencap -p /sdcard/screenshot.png
+	$ADB_DEVICE pull /sdcard/screenshot.png $LOGPATH
+}
+
+
+while getopts "r:n:l:s:" arg
+do
+	case $arg in
+	l ) #echo $OPTARG
+		DATETIME=$(echo $OPTARG | cut -d _ -f 3 | cut -d . -f 1)
+		#echo $DATETIME
+	;;
+	r ) #echo $OPTARG
+		if test -z "$DATETIME"
+		then
+			RUN_TIME=$OPTARG
+		fi
+		#echo 'RUN_TIME:'$RUN_TIME
+	;;
+	n ) #echo $OPTARG
+		if test -z "$DATETIME"
+		then
+			DEVICE_SN=$OPTARG
+			ADB_DEVICE=“adb -s “$DEVICE_SN
+		fi
+		#echo 'DEVICE_SN:'$DEVICE_SN
+	;;
+	s ) #echo $OPTARG
+		if test -z "$DATETIME"
+		then
+			MONKEY_SEED='-s '$OPTARG
+		fi
+		#echo 'MONKEY_SEED:'$MONKEY_SEED
+	;;
+	? ) echo usage:
+		echo script will run 1 time if no arg found
+		echo -l \<monkey log file\ >				- analyse monkey log file. log file name should be monkey_log_yyyymmdd-hhmmss.txt
+		echo -r \<monkey event count\ >			- specified monkey test event count
+		echo -n \<device hardware sn\ >			- specified device sn for ads
+		echo -s \<monkey test seed\ >				- specified monkey test seed
+		exit 1
+	;;
+	esac
+done
+
+#echo 'DATETIME:'$DATETIME
+# process monkey log file only
+if test -n "$DATETIME"
+then
+	echo "*****analyse monkey log*****"
+	RANDOM_SEED=$(cat monkey_log_$DATETIME.txt | grep -o 'seed=[0-9]*' | cut -d = -f 2)
+	EVENT_COUNT=$(cat monkey_log_$DATETIME.txt | grep -o 'Events injected: [0-9]*' | awk '{print $3}')
+	if test -z "$EVENT_COUNT" ;then
+		EVENT_COUNT=0
+		unknownCat
 	fi
 	echo 'random seed:'$RANDOM_SEED
 	echo 'event count:'$EVENT_COUNT
@@ -41,53 +133,27 @@ then
 	echo 'event exercuted:'$EVENT_EXERCUTED
 	ERROR_INFO=$(cat monkey_log_$DATETIME.txt | grep 'ANR in')
 	if test -n "$ERROR_INFO" ;then
-		echo 'error cause:ANR'
-		mkdir -p ANR
-		cd ANR
-		ERROR_POS=$(echo $ERROR_INFO | cut -d \/ -f 2 | cut -d \) -f 1 | cut -d . -f 2)
-		ERROR_INFO=$(echo $ERROR_INFO | awk '{print $3}')
-		mkdir -p $ERROR_INFO
-		cd $ERROR_INFO
-		mkdir -p $ERROR_POS
-		cd $ERROR_POS
-		mkdir -p $DATETIME
-		cd ..
-		cd ..
-		cd ..
-		mv monkey_log_$DATETIME.txt ANR/$ERROR_INFO/$ERROR_POS/$DATETIME/
-		$ADB_DEVICE pull /data/tombstones/ ANR/$ERROR_INFO/$ERROR_POS/$DATETIME/
-		$ADB_DEVICE pull /data/system/dropbox/ ANR/$ERROR_INFO/$ERROR_POS/$DATETIME/
-		$ADB_DEVICE shell rm -f /data/tombstones/*
-		$ADB_DEVICE shell rm -f /data/system/dropbox/*
+		anrCat
 	fi
 	if test -z "$ERROR_INFO" ;then
 		ERROR_INFO=$(cat monkey_log_$DATETIME.txt | grep 'CRASH:')
 		if test -n "$ERROR_INFO" ;then
-			ERROR_POS=$(cat monkey_log_$DATETIME.txt | grep '.java:' | head -1 | awk '{printf $3}')
-			if test -n "$ERROR_POS" ;then
-				echo 'error cause:CRASH'
-				echo 'error position:'$ERROR_POS
-				mkdir -p CRASH
-				cd CRASH
-				mkdir -p $ERROR_POS
-				cd $ERROR_POS
-				mkdir -p $DATETIME
-				cd ..
-				cd ..
-				mv monkey_log_$DATETIME.txt CRASH/$ERROR_POS/$DATETIME/
-				$ADB_DEVICE pull /data/tombstones/ CRASH/$ERROR_POS/$DATETIME/
-				$ADB_DEVICE pull /data/system/dropbox/ CRASH/$ERROR_POS/$DATETIME/
-				$ADB_DEVICE shell rm -f /data/tombstones/*
-				$ADB_DEVICE shell rm -f /data/system/dropbox/*
-			fi
+			crashCat
+		fi
+		if test -z "$ERROR_INFO" ;then
+			normalCat
 		fi
 	fi
 
-	echo 'error cause:'$ERROR_INFO
-fi
-if [ $# -eq 0 ]
-then
-	while [ $EVENT_EXERCUTED -le $RUN_TIME ]
+	# run monkey for specified events count
+else
+	if test -z “$RUN_TIME”
+	then
+		echo RUN_TIME is null
+		exit 1
+	fi
+
+	while test $EVENT_EXERCUTED -lt $RUN_TIME
 	do
 		# reboot devices for next run
 		echo "*****reboot device*****"
@@ -96,6 +162,7 @@ then
 		# wait for device connection
 		echo "*****wait for device*****"
 		$ADB_DEVICE wait-for-device
+		$ADB_DEVICE push blacklist.txt /data/
 		# run monkey with a random seed
 		if test -z "$MONKEY_SEED" ;then
 			echo "*****run monkey with a random seed*****"
@@ -104,25 +171,15 @@ then
 			echo "****run monkey with specified seed" $MONKEY_SEED "*****"
 		fi
 		DATETIME=`date +%Y%m%d-%H%M%S`
-		#DATETIME='20160505-211209'
 		$ADB_DEVICE shell monkey --pkg-blacklist-file /data/blacklist.txt --pct-trackball 0 $MONKEY_SEED -v -v -v $RUN_TIME > monkey_log_$DATETIME.txt
 		# analyse monkey log, figure out error catagory and pull log to pc
 		echo "*****analyse monkey log*****"
 		RANDOM_SEED=$(cat monkey_log_$DATETIME.txt | grep -o 'seed=[0-9]*' | cut -d = -f 2)
-		EVENT_COUNT=$(cat monkey_log_$DATETIME.txt | grep -o 'at event [0-9]*' | awk '{print $3}')
+		EVENT_COUNT=$(cat monkey_log_$DATETIME.txt | grep -o 'Events injected: [0-9]*' | awk '{print $3}')
 		if test -z "$EVENT_COUNT" ;then
 			EVENT_COUNT=0
-			mkdir -p unknown
-			cd unknown
-			mkdir -p $DATETIME
-			cd ..
-			mv monkey_log_$DATETIME.txt unknown/$DATETIME
-			$ADB_DEVICE pull /data/tombstones/ unknown/$DATETIME/
-			$ADB_DEVICE pull /data/system/dropbox/ unknown/$DATETIME/
-			$ADB_DEVICE shell /system/bin/screencap -p /sdcard/screenshot.png
-			$ADB_DEVICE pull /sdcard/screenshot.png unknown/$DATETIME/
-			$ADB_DEVICE shell rm -f /data/tombstones/*
-			$ADB_DEVICE shell rm -f /data/system/dropbox/*
+			unknownCat
+			pullLogAndRemove
 		fi
 		echo 'random seed:'$RANDOM_SEED
 		echo 'event count:'$EVENT_COUNT
@@ -130,51 +187,21 @@ then
 		echo 'event exercuted:'$EVENT_EXERCUTED
 		ERROR_INFO=$(cat monkey_log_$DATETIME.txt | grep 'ANR in')
 		if test -n "$ERROR_INFO" ;then
-			echo 'error cause:ANR'
-			mkdir -p ANR
-			cd ANR
-			ERROR_POS=$(echo $ERROR_INFO | cut -d \/ -f 2 | cut -d \) -f 1 | cut -d . -f 2)
-			ERROR_INFO=$(echo $ERROR_INFO | awk '{print $3}')
-			mkdir -p $ERROR_INFO
-			cd $ERROR_INFO
-			mkdir -p $ERROR_POS
-			cd $ERROR_POS
-			mkdir -p $DATETIME
-			cd ..
-			cd ..
-			cd ..
-			mv monkey_log_$DATETIME.txt ANR/$ERROR_INFO/$ERROR_POS/$DATETIME/
-			$ADB_DEVICE pull /data/tombstones/ ANR/$ERROR_INFO/$ERROR_POS/$DATETIME/
-			$ADB_DEVICE pull /data/system/dropbox/ ANR/$ERROR_INFO/$ERROR_POS/$DATETIME/
-			$ADB_DEVICE shell /system/bin/screencap -p /sdcard/screenshot.png
-			$ADB_DEVICE pull /sdcard/screenshot.png ANR/$ERROR_INFO/$ERROR_POS/$DATETIME/
-			$ADB_DEVICE shell rm -f /data/tombstones/*
-			$ADB_DEVICE shell rm -f /data/system/dropbox/*
+			anrCat
+			pullLogAndRemove
 		fi
 		if test -z "$ERROR_INFO" ;then
 			ERROR_INFO=$(cat monkey_log_$DATETIME.txt | grep 'CRASH:')
 			if test -n "$ERROR_INFO" ;then
-				ERROR_POS=$(cat monkey_log_$DATETIME.txt | grep '.java:' | head -1 | awk '{printf $3}')
-				if test -n "$ERROR_POS" ;then
-					echo 'error cause:CRASH'
-					echo 'error position:'$ERROR_POS
-					mkdir -p CRASH
-					cd CRASH
-					mkdir -p $ERROR_POS
-					cd $ERROR_POS
-					mkdir -p $DATETIME
-					cd ..
-					cd ..
-					mv monkey_log_$DATETIME.txt CRASH/$ERROR_POS/$DATETIME/
-					$ADB_DEVICE pull /data/tombstones/ CRASH/$ERROR_POS/$DATETIME/
-					$ADB_DEVICE pull /data/system/dropbox/ CRASH/$ERROR_POS/$DATETIME/
-					$ADB_DEVICE shell /system/bin/screencap -p /sdcard/screenshot.png
-					$ADB_DEVICE pull /sdcard/screenshot.png CRASH/$ERROR_POS/$DATETIME/
-					$ADB_DEVICE shell rm -f /data/tombstones/*
-					$ADB_DEVICE shell rm -f /data/system/dropbox/*
-				fi
+				crashCat
+				pullLogAndRemove
+			fi
+			if test -z "$ERROR_INFO" ;then
+				normalCat
 			fi
 		fi
 
 	done
 fi
+
+
